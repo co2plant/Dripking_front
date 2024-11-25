b614307fb45bdbbcd984acee26aa00b18d24dbf1
JO YOUNGJAE <93631841+co2plant@users.noreply.github.com>
2024-11-25 23:00:22 +0900

    feat : 백엔드(RESTful) 서버 통신을 위한 axios 임포트 및 페이지 추가

---
+++ src/WhereToGo.vue
@@ -0,0 +1,30 @@
+<template>
+  <Header/>
+  <LocationLists/>
+  <Footer/>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+import axios from 'axios';
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+import LocationLists from "@/components/LocationLists.vue";
+
+onMounted(() => {
+  axios.get("/api/items").then((response) => {
+    console.log(response);
+  });
+});
+</script>
+
+<style>
+#whereToGo {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import WhereToGo from './WhereToGo.vue'
 
 createApp(App).mount('#app')
+createApp(WhereToGo).mount('#whereToGo')
