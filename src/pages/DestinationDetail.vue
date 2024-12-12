51524a3a19ea2e1023c005acfe97e6945b212084
JO YOUNGJAE <93631841+co2plant@users.noreply.github.com>
2024-12-12 22:56:39 +0900

    feat : router control 추가

---
+++ src/pages/DestinationDetail.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import Footer from "@/components/Footer.vue";
+  import Header from "@/components/Header.vue";
+  import Destination from "@/components/Destination.vue";
+</script>
+
+<template>
+  <Header/>
+  <Destination/>
+  <Footer/>
+</template>
+
+<style scoped>
+
+</style>
