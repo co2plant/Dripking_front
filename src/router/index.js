a042ba17089357823586e29751d82d0fa209e411
JO YOUNGJAE <93631841+co2plant@users.noreply.github.com>
2024-12-12 22:56:44 +0900

    feat : router control 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,38 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Destination from '../components/Destination.vue'
-import Distilleries from '../components/Distilleries.vue'
+
+import DestinationList from "@/pages/DestinationList.vue";
+import DistilleryList from "@/pages/DistilleryList.vue";
+import DistilleryDetail from "@/pages/DistilleryDetail.vue";
+import DestinationDetail from "@/pages/DestinationDetail.vue";
 
 const routes = [
-    { path: '/', component: Destination },
-    { path: '/distillery/:id', name: 'DistilleryDetail', component: Distilleries }
+    { path: '/destinationList', name:'DestinationList', component: DestinationList },
+    { path: '/destination/:id', name: 'DestinationDetail', component: DestinationDetail },
+    { path: '/distilleryList', name: 'distilleryList', component : DistilleryList },
+    { path: '/distillery/:id', name: 'DistilleryDetail', component: DistilleryDetail }
 ]
 
 const router = createRouter({
     history: createWebHistory(),
-    routes
+    routes,
 })
 
 export default router
+
+/*
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+import About from '../views/About.vue';
+
+const routes = [
+  { path: '/', component: Home }, // 루트 경로
+  { path: '/about', component: About }, // /about 경로
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
+ */
