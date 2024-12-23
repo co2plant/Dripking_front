68999bf4f126228665f86ba86fc403d5ca749458
JO YOUNGJAE <93631841+co2plant@users.noreply.github.com>
2024-12-23 22:17:15 +0900

    fix : 중복되는 vue 삭제

--- src/views/DestinationDetail.vue
+++ src/views/DestinationDetail.vue
@@ -1,16 +1,37 @@
-<script setup>
-  import Destination from "@/components/Destination.vue";
-</script>
-
 <template>
-  <Destination/>
+  <div class="bg-white">
+    <div class="container mx-auto px-4 py-8">
+      <Card
+          :toName = "'DestinationDetail'"
+          :urlStr ="urlStr"
+      >
+      </Card>
+
+      <Horizontal-scroll-card-list
+          :title="'주변 양조장'"
+          :toName="'distilleryDetail'"
+          :urlStr="'api/distilleries'">
+      </Horizontal-scroll-card-list>
+    </div>
+  </div>
 </template>
 
-<script>
-  export default{
-    name: "destinationDetail"
-  }
+<script setup>
+import HorizontalScrollCardList from "@/components/HorizontalScrollCardList.vue";
+import Card from "@/components/Card.vue";
+import {useRoute} from "vue-router";
+
+const route = useRoute();
+const urlStr = 'destination/'+route.params.id;
 </script>
 
 <style scoped>
+.scrollbar-hide {
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  -webkit-overflow-scrolling: touch;
+}
+.scrollbar-hide::-webkit-scrollbar {
+  display: none;
+}
 </style>
