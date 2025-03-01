8cd9116b75fad529d5dc97d2b2ae99d4bb3e18c8
co2plant <93631841+co2plant@users.noreply.github.com>
2025-03-01 23:30:53 +0900

    Fix : 각 엔티티 클래스 할당(임시)

--- src/composables/useWishlist.js
+++ src/composables/useWishlist.js
@@ -1,4 +1,10 @@
 import { ref, watch, onMounted } from 'vue';
+import Trip from "@/composables/Entity/Trip";
+import Plan from "@/composables/Entity/Plan";
+import Alcohol from "@/composables/Entity/Alcohol";
+import Destination from "@/composables/Entity/Destination";
+import Distillery from "@/composables/Entity/Distillery";
+
 
 export function useWishlist() {
     const WishlistItems = ref([]);
@@ -14,23 +20,77 @@ export function useWishlist() {
         localStorage.setItem('Wishlist', JSON.stringify(newItems));
     };
 
+    const getIndexOfWishlistItem = (item) => {
+        return WishlistItems.value.findIndex(wishItem => wishItem.id === item.id && wishItem.itemType === item.itemType)
+    }
+
     const toggleWishlist = (item) => {
-        const index = WishlistItems.value.findIndex(wishItem => wishItem.id === item.id && wishItem.itemType === item.itemType)
+        const index = getIndexOfWishlistItem(item)
         if (index === -1) {
-            const newItem = {
-                id: item.id,
-                trip_id: -1,
-                plan_id: -1,
-                itemType: item.itemType,
-                name: item.name,
-                description: item.description,
-                img_url: item.img_url
+            if(item.itemType === 'TRIP'){
+                const newItem = new Trip();
+                newItem
+                    .setId(item.id)
+                    .setName(item.name)
+                    .setDescription(item.description)
+                    .setStartDate(item.start_date)
+                    .setEndDate(item.end_date)
+                    .setIsLocal(item.isLocal)
+                    .setCountry(item.country)
+                    .build();
+                WishlistItems.value.push(newItem);
+            }else if(item.itemType === 'PLAN'){
+                const newItem = new Plan();
+                newItem
+                    .setId(item.id)
+                    .setName(item.name)
+                    .setDescription(item.description)
+                    .setPlanDate(item.plan_date)
+                    .setStartTime(item.start_time)
+                    .setEndTime(item.end_time)
+                    .setPlaceId(item.place_id)
+                    .setTripId(item.trip_id)
+                    .build();
+                WishlistItems.value.push(newItem);
+            }else if(item.itemType === 'ALCOHOL'){
+                const newItem = new Alcohol();
+                newItem
+                    .setId(item.id)
+                    .setName(item.name)
+                    .setDescription(item.description)
+                    .setImgUrl(item.img_url)
+                    .build();
+                WishlistItems.value.push(newItem);
+            }else if(item.itemType === 'DISTILLERY'){
+                const newItem = new Distillery();
+                newItem
+                    .setId(item.id)
+                    .setName(item.name)
+                    .setDescription(item.description)
+                    .setImgUrl(item.img_url)
+                    .setLatitude(item.latitude)
+                    .setLongitude(item.longitude)
+                    .build();
+                WishlistItems.value.push(newItem);
+            }else if(item.itemType === 'DESTINATION'){
+                const newItem = new Destination();
+                newItem
+                    .setId(item.id)
+                    .setName(item.name)
+                    .setDescription(item.description)
+                    .setImgUrl(item.img_url)
+                    .setLatitude(item.latitude)
+                    .setLongitude(item.longitude)
+                    .build();
+                WishlistItems.value.push(newItem);
+            }else{
+                return false;
             }
-            WishlistItems.value.push(newItem)
         } else {
             WishlistItems.value.splice(index, 1)
         }
         WishlistItems.value.sort((a, b) => a.trip_id - b.trip_id);
+        return true;
     };
 
     /* Wishlist에서 Trip을 삭제할때 Trip에 속한 Plan도 같이 삭제
@@ -45,27 +105,34 @@ export function useWishlist() {
         });
     }
 
-    /* Wishlis에서 특정 plan의 trip_id를 변경할 때 사용
-     * @param item_trip_id, item_id, update_trip_id
+    const setTripId = (item, trip_id) => {
+        const index = getIndexOfWishlistItem(item)
+        if (index !== -1) {
+            WishlistItems.value[index].trip_id = Number(trip_id)
+        }
+
+    }
+    const setPlanId = (item, plan_id) => {
+        const index = getIndexOfWishlistItem(item)
+        if (index !== -1) {
+            WishlistItems.value[index].plan_id = Number(plan_id)
+        }
+
+    }
+
+    /* Wishlist에서 특정 plan의 trip_id를 변경할 때 사용
+     * @param
+     * item_trip_id : 현재 item의 trip_id,
+     * item_id : 현재 item의 id,
+     * update_trip_id : 현재 item의 바뀔 trip_id
      */
     const toggleWishlistUpdatePlanID = (item_trip_id, item_id, update_trip_id) => {
         const index = WishlistItems.value.findIndex(wishItem => wishItem.id === Number(item_id) && wishItem.trip_id === Number(item_trip_id))
-        const item = WishlistItems.value[index]
 
-        const newItem = {
-            id: item.id,
-            trip_id: Number(update_trip_id),
-            plan_id: -1,
-            itemType: item.itemType,
-            name: item.name,
-            description: item.description,
-            img_url: item.img_url
-        }
         if (index === -1) {
             return
         } else {
-            WishlistItems.value.splice(index, 1)
-            WishlistItems.value.push(newItem)
+            WishlistItems.value[index].trip_id = Number(update_trip_id)
         }
         WishlistItems.value.sort((a, b) => a.trip_id - b.trip_id);
     };
@@ -86,6 +153,8 @@ export function useWishlist() {
         toggleWishlistUpdatePlanID,
         toggleWishlist,
         isInWishlist,
-        deleteAllFromWishlistByTripId
+        deleteAllFromWishlistByTripId,
+        setTripId,
+        setPlanId
     };
 }
