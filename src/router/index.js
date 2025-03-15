f14112820d02b8a8860ea62a7ec9cff782b22909
co2plant <93631841+co2plant@users.noreply.github.com>
2025-03-15 11:16:30 +0900

    Fix : for Deploy without error

--- src/router/index.js
+++ src/router/index.js
@@ -9,8 +9,10 @@ import AlcoholDetail from "@/views/AlcoholDetail.vue";
 import SearchedList from "@/views/SearchedList.vue";
 import TestPage from "@/views/TestPage.vue";
 import TripModify from "@/views/TripModify.vue";
-import Map from  "@/components/map.vue";
+// import Map from  "@/components/map.vue";
+import EditUserDetail from "@/views/EditUserDetail.vue"
 import {useAuthStore} from "@/stores/useAuthStore";
+
 
 const routes = [
     { path: '/', name: 'Home', component: Landing },
@@ -23,8 +25,8 @@ const routes = [
     { path: '/search/:dtype', name: 'searchList', component: SearchedList, meta:{requiredGuest : true}}, //현재는 미사용(검색기능 추가 시 사용할 예정)
     { path: '/test', name:'test', component:TestPage, meta:{requiredGuest : true}},
     { path: '/trip/:id', name:'tripModify', component: TripModify, meta:{requiredGuest : true}},
-    { path: '/map', name:'map', component: Map, meta:{requiredGuest : true}},
-    { path: '/editUserDetail', name:'editUserDetail', component: () => import('@/views/EditUserDetail.vue'), meta:{requiredUser : true}},
+    // { path: '/map', name:'map', component: Map, meta:{requiredGuest : true}},
+    { path: '/editUserDetail', name:'editUserDetail', component: EditUserDetail },
 ]
 
 const router = createRouter({
