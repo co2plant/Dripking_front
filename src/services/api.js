b4eeeb99a7b2e3f2e7020d297f1a2e3ab74b4c4f
co2plant <93631841+co2plant@users.noreply.github.com>
2025-03-27 22:48:03 +0900

    refactor : refactoring fetch api

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,15 @@
-const API_URL = process.env.VUE_APP_API_URL || 'http://localhost:8080/api';
+const API_BASE_URL = process.env.VUE_APP_API_URL || 'http://localhost:8080/api';
 
 export const apiService = {
     async get(endpoint){
-        const response = await fetch(`${API_URL}/${endpoint}`)
+        const response = await fetch(`${API_BASE_URL}/${endpoint}`)
             .then(response => response.json()
         );
         return await response;
     },
 
     async post(endpoint, data){
-        const response = await fetch(`${API_URL}/${endpoint}`, {
+        const response = await fetch(`${API_BASE_URL}/${endpoint}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -20,14 +20,37 @@ export const apiService = {
     },
 
     async postWithToken(endpoint, data){
-        const response = await fetch(`${API_URL}/${endpoint}`, {
+        const response = await fetch(`${API_BASE_URL}/${endpoint}`, {
             method: 'POST',
-            header: {
+            headers: {
                 'Content-Type': 'application/json',
-                'Authorization' : 'Bearer ' + localStorage.getItem('token')
+                'Authorization' : localStorage.getItem('Authorization')
             },
             body: JSON.stringify(data)
         });
         return await response.json();
-    }
+    },
+
+    async put(endpoint, data){
+        const response = await fetch(`${API_BASE_URL}/${endpoint}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        return await response.json();
+    },
+
+    async putWithToken(endpoint, data){
+        const response = await fetch(`${API_BASE_URL}/${endpoint}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization' : localStorage.getItem('Authorization')
+            },
+            body: JSON.stringify(data)
+        });
+        return await response.json();
+    },
 }
