7a85f1d57988995f50eae1d2bd5fdd472ffa2147
co2plant <jaonz6057@wku.ac.kr>
2025-06-02 16:23:00 +0900

    Fix : 데이터 받자마자 ItemType 할당하도록 변경

--- src/stores/useWishStore.js
+++ src/stores/useWishStore.js
@@ -1,4 +1,8 @@
 import { defineStore } from 'pinia';
+import Alcohol from '@/composables/Entity/Alcohol'
+import Distillery from '@/composables/Entity/Distillery'
+import Destination from '@/composables/Entity/Destination'
+import Place from '@/composables/Entity/Place'
 
 export const useWishStore = defineStore('wishlist', {
     state: () => ({WishItems:[]}),
@@ -15,6 +19,7 @@ export const useWishStore = defineStore('wishlist', {
                 localStorage.setItem('Wishlist', JSON.stringify(this.WishItems));
             }
         },
+        //현재 사용 X
         addWishItem(newWishItem){
             if(newWishItem!==null){
                 this.WishItems.push(newWishItem);
@@ -23,17 +28,67 @@ export const useWishStore = defineStore('wishlist', {
         removeWishItem(wishItemId){
             this.WishItems = this.WishItems.filter(wishItem => wishItem.id !== wishItemId);
         },
-        toggleWishlist(item){
-            const index = this.WishItems.findIndex(wishItem => wishItem.id === item.id && wishItem.itemType === item.itemType);
-            if(index === -1){
-                this.WishItems.push(item);
-            }else{
+        toggleWishlist(item, itemType) {
+            let entity;
+            switch (itemType) {
+                case 'ALCOHOL':
+                    entity = new Alcohol()
+                        .setId(item.id)
+                        .setName(item.name)
+                        .setDescription(item.description)
+                        .setImgUrl(item.img_url)
+                        .setPlanId(item.plan_id)
+                        .build();
+                    break;
+                case 'DISTILLERY':
+                    entity = new Distillery()
+                        .setId(item.id)
+                        .setName(item.name)
+                        .setLatitude(item.latitude)
+                        .setLongitude(item.longitude)
+                        .setAddress(item.address)
+                        .setDescription(item.description)
+                        .setImgUrl(item.img_url)
+                        .setPlanID(item.plan_id)
+                        .build();
+                    break;
+                case 'DESTINATION':
+                    entity = new Destination()
+                        .setId(item.id)
+                        .setName(item.name)
+                        .setLatitude(item.latitude)
+                        .setLongitude(item.longitude)
+                        .setDescription(item.description)
+                        .setImgUrl(item.img_url)
+                        .setPlanID(item.plan_id)
+                        .build();
+                    break;
+                case 'PLACE':
+                    entity = new Place()
+                        .setId(item.id)
+                        .setName(item.name)
+                        .setLatitude(item.latitude)
+                        .setLongitude(item.longitude)
+                        .setAddress(item.address)
+                        .setDescription(item.description)
+                        .setImgUrl(item.img_url)
+                        .setPlanID(item.plan_id)
+                        .build();
+                    break;
+                default:
+                    entity = { ...item, itemType };
+            }
+
+            const index = this.WishItems.findIndex(wishItem => wishItem.id === entity.id && wishItem.itemType === itemType);
+            if (index === -1) {
+                this.WishItems.push(entity);
+            } else {
                 this.WishItems.splice(index, 1);
             }
             this.saveWishlist();
         },
-        isInWishlist(item){
-            return this.WishItems.some(wishItem => wishItem.id === item.id && wishItem.itemType === item.itemType);
+        isInWishlist(item, itemType){
+            return this.WishItems.some(wishItem => wishItem.id === item.id && wishItem.itemType === itemType);
         },
         sortWishlist(){
             return [...this.WishItems].sort((a, b) => {
