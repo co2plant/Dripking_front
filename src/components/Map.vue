12d8f8896ec0535e03a99d8b4d157a37a6ce4d74
co2plant <jaonz6057@wku.ac.kr>
2025-06-03 23:16:24 +0900

    Feat : Plan에 저장된 내용이 marker에 뜨도록 변경

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -87,7 +87,7 @@ async function initMap() {
     });
 
     let number = 0;
-    for (const property of properties) {
+    for (const property of planStore.Plans) {
       const markerContent = buildContent(property, number++);
       const marker = new AdvancedMarkerElement({
         map: mapInstance,
@@ -153,129 +153,16 @@ function buildContent(property, number) {
   return content;
 }
 
-const properties = [
-  {
-    id: 1,
-    itemType: 'PLACE',
-    name: "Emily St",
-    latitude: 37.50024109655184,
-    longitude: -122.28528451834352,
-    description: "Single family house with modern design",
-    address: "215 Emily St, MountainView, CA",
-    img_url: null,
-    plan_id: -1
-  },
-  {
-    id: 2,
-    itemType: 'DESTINATION',
-    name: "Squirrel Ln",
-    latitude: 37.44440882321596,
-    longitude: -122.2160620727,
-    description: "Townhouse with friendly neighbors",
-    address: "108 Squirrel Ln, Menlo Park, CA",
-    img_url: null,
-    plan_id: -1
-  },
-  {
-    id: 3,
-    itemType: 'DISTILLERY',
-    name: "Chris St",
-    latitude: 37.39561833718522,
-    longitude: -122.21855116258479,
-    description: "Spacious warehouse great for small business",
-    address: "100 Chris St, Portola Valley, CA",
-    img_url: null,
-    plan_id: -1
-  },
-  {
-    id: 4,
-    itemType: 'PLACE',
-    name: "Aleh Ave",
-    latitude: 37.423928529779644,
-    longitude: -122.1087629822001,
-    description: "A lovely store on busy road",
-    address: "98 Aleh Ave, Palo Alto, CA",
-    img_url: null,
-    plan_id: -1
-  },
-  {
-    id: 5,
-    itemType: 'PLACE',
-    name: "Su St",
-    latitude: 37.40578635332598,
-    longitude: -122.15043378466069,
-    description: "Single family house near golf club",
-    address: "2117 Su St, MountainView, CA",
-    img_url: null,
-    plan_id: -1
-  },
-  {
-    id: 6,
-    itemType: 'PLACE',
-    name: "Alicia Dr",
-    latitude: 37.36399747905774,
-    longitude: -122.10465384268522,
-    description: "Multifloor large warehouse",
-    address: "197 Alicia Dr, Santa Clara, CA",
-    img_url: null,
-    plan_id: -1
-  },
-  {
-    id: 7,
-    itemType: 'PLACE',
-    name: "Jose Ave",
-    latitude: 37.38343706184458,
-    longitude: -122.02340436985183,
-    description: "3 storey townhouse with 2 car garage",
-    address: "700 Jose Ave, Sunnyvale, CA",
-    img_url: null,
-    plan_id: -1
-  },
-  {
-    id: 8,
-    itemType: 'PLACE',
-    name: "Will Ct",
-    latitude: 37.34576403052,
-    longitude: -122.04455090047453,
-    description: "Single family house in great school zone",
-    address: "868 Will Ct, Cupertino, CA",
-    img_url: null,
-    plan_id: -1
-  },
-  {
-    id: 9,
-    itemType: 'PLACE',
-    name: "Haylee St",
-    latitude: 37.362863347890716,
-    longitude: -121.97802139023555,
-    description: "2 storey store with large storage room",
-    address: "655 Haylee St, Santa Clara, CA",
-    img_url: null,
-    plan_id: -1
-  },
-  {
-    id: 10,
-    itemType: 'PLACE',
-    name: "Natasha Dr",
-    latitude: 37.41391636421949,
-    longitude: -121.94592071575907,
-    description: "Single family house",
-    address: "2019 Natasha Dr, San Jose, CA",
-    img_url: null,
-    plan_id: -1
-  },
-];
-
-
 onMounted(() => {
   planStore.loadPlans();
   tripStore.loadTrips();
+
   getMapCenterByFirstPlace();
 
-  if (planStore.plan && planStore.plan.length > 0) {
+  if (planStore.Plans && planStore.Plans.length > 0) {
     map_center.value = {
-      lat: planStore.plan[0].place_id,
-      lng: planStore.plan[0].lng
+      lat: planStore.Plans[0].latitude,
+      lng: planStore.Plans[0].longitude
     }
   }
 
